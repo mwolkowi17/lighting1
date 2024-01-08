4f2299719ed0ab58e0c2b9c8526c2799de31d6dd
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2024-01-08 13:27:41 +0100

    final light not good

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 const scene = new THREE.Scene()
 
@@ -11,9 +12,38 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.z = 2
 
+const hemisphere = new THREE.HemisphereLight(0xfeeb1,0x08020, 20)
+scene.add(hemisphere)
+
+const spot1 = new THREE.SpotLight(0xffa95c,40)
+spot1.castShadow = true
+spot1.shadow.bias = -0.0001
+spot1.shadow.mapSize.width = 1024*4
+spot1.shadow.mapSize.height= 1024*4
+
+scene.add(spot1)
+
 const renderer = new THREE.WebGLRenderer()
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 2.3
+renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+new GLTFLoader().load('./sprzeglo_klowe_for_app.glb', (gltf) => {
+    const root = gltf.scene;
+    root.scale.x=.5
+    root.scale.y=.5
+    root.scale.z=.5
+    
+    root.traverse(n=>{  
+        n.castShadow=true
+        n.receiveShadow=true
+    })
+
+    scene.add(root); //co zrobic z tym?)
+}
+)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
@@ -24,7 +54,7 @@ const material = new THREE.MeshBasicMaterial({
 })
 
 const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+//scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -39,6 +69,11 @@ function animate() {
     controls.update()
     //cube.rotation.x += 0.01
     //cube.rotation.y += 0.01
+    spot1.position.set(
+        camera.position.x+10,
+        camera.position.y+10,
+        camera.position.z+10,
+    )
 
     render()
 }
